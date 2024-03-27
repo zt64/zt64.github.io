3ef328fd9acd3bf877f508ede8436d3e29311938
zt64 <31907977+zt64@users.noreply.github.com>
2024-03-26 23:49:44 -0400

    apply ktlint formatting

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -14,7 +14,7 @@ fun main() {
                 alignItems(AlignItems.Center)
                 height(100.vh)
             },
-            gap = 6.cssRem,
+            gap = 6.cssRem
         ) {
             Img(
                 attrs = {
@@ -24,11 +24,11 @@ fun main() {
                         borderRadius(5.cssRem)
                     }
                 },
-                src = "https://avatars.githubusercontent.com/u/31907977?v=4",
+                src = "https://avatars.githubusercontent.com/u/31907977?v=4"
             )
 
             Column(
-                gap = 1.1.cssRem,
+                gap = 1.1.cssRem
             ) {
                 H1(
                     attrs = {
@@ -36,7 +36,7 @@ fun main() {
                             fontFamily("Space Grotesk")
                             fontSize(6.cssRem)
                         }
-                    },
+                    }
                 ) {
                     Text("hey, i'm ")
 
@@ -45,7 +45,7 @@ fun main() {
                             style {
                                 fontWeight(700)
                             }
-                        },
+                        }
                     ) {
                         Text("zt")
                     }
@@ -57,23 +57,23 @@ fun main() {
                             color(Color("#E8E8E8"))
                             width(30.cssRem)
                         }
-                    },
+                    }
                 ) {
                     Text(
                         """
                         I'm a 20yo student studying Applied Computer Science who likes making random things. 
                         Currently I’m working on a YouTube client for Android called Hyperion.
-                        """.trimIndent(),
+                        """.trimIndent()
                     )
                 }
 
                 List(
-                    gap = 1.2.cssRem,
+                    gap = 1.2.cssRem
                 ) {
                     SocialChip(
                         icon = { Icon("icons/github.svg") },
                         label = { Text("Github") },
-                        url = "https://github.com/zt64",
+                        url = "https://github.com/zt64"
                     )
                 }
             }
