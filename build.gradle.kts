2f6eab72cb9e1e66d2cdaf218926743ed73b4dab
zt64 <31907977+zt64@users.noreply.github.com>
2024-05-17 23:42:47 -0400

    update kotlin and compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.serialization)
-    alias(libs.plugins.compose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose.jb)
     alias(libs.plugins.ktlint)
 }
 
